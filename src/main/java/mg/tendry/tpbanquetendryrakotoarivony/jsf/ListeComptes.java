1eba339462afc434fc6589ae316de0608c4daccf
Tendry Rakotoarivony <tendryarivony@gmail.com>
2024-04-16 23:00:07 +0300

    Pages jsf et back bean associés

---
+++ src/main/java/mg/tendry/tpbanquetendryrakotoarivony/jsf/ListeComptes.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.tendry.tpbanquetendryrakotoarivony.jsf;
+
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
+import mg.tendry.tpbanquetendryrakotoarivony.entity.CompteBancaire;
+import mg.tendry.tpbanquetendryrakotoarivony.service.GestionnaireCompte;
+
+/**
+ *
+ * @author Tendry Arivony
+ */
+@Named(value = "listeComptes")
+@ViewScoped
+public class ListeComptes implements Serializable {
+
+    private List<CompteBancaire> compteList;
+
+    @Inject
+    private GestionnaireCompte gestionnaireCompte;
+
+    /**
+     * Creates a new instance of ListeComptes
+     */
+    public ListeComptes() {
+    }
+
+    /**
+     * Retourne la liste des comptes pour affichage dans une DataTable.
+     *
+     * @return
+     */
+    public List<CompteBancaire> getAllComptes() {
+        if (compteList == null) {
+            compteList = gestionnaireCompte.getAllComptes();
+        }
+        return compteList;
+    }
+}
